a5d9be27ee6afc4be766542acc8379ba5a810455
David Hiendl <david.hiendl@dhswt.de>
2018-04-02 00:53:12 +0200

    reconfigured config defaults to use envconfig built-in defaults

--- app/config/config.go
+++ app/config/config.go
@@ -8,46 +8,29 @@ import (
 type Config struct {
 	S3AccessKey  string `envconfig:"S3_ACCESS_KEY"`
 	S3SecretKey  string `envconfig:"S3_SECRET_KEY"`
-	S3Endpoint   string `envconfig:"S3_ENDPOINT"`
-	S3Region     string `envconfig:"S3_REGION"`
+	S3Endpoint   string `envconfig:"S3_ENDPOINT" default:"s3.us-west-1.amazonaws.com"`
+	S3Region     string `envconfig:"S3_REGION" default:"us-west-1"`
 	S3Bucket     string `envconfig:"S3_BUCKET"`
-	S3PathPrefix string `envconfig:"S3_PATH_PREFIX"`
+	S3PathPrefix string `envconfig:"S3_PATH_PREFIX" default:""`
 
-	MySQLDumpBinary string `envconfig:"MYSQLDUMP_BINARY"`
+	MySQLDumpBinary string `envconfig:"MYSQLDUMP_BINARY" default:"mysqldump"`
 
-	MySQLUser string `envconfig:"MYSQL_USER"`
+	MySQLUser string `envconfig:"MYSQL_USER" default:"root"`
 	MySQLPass string `envconfig:"MYSQL_PASS"`
-	MySQLHost string `envconfig:"MYSQL_HOST"`
-	MySQLPort int    `envconfig:"MYSQL_PORT"`
+	MySQLHost string `envconfig:"MYSQL_HOST" default:"127.0.0.1"`
+	MySQLPort int    `envconfig:"MYSQL_PORT" default:"3306"`
 
-	ExportTempDir       string `envconfig:"EXPORT_TEMP_DIR"`
-	SkipSystemDatabases bool   `envconfig:"SKIP_SYSTEM_DATABASES"`
-	CompressWithGz      bool   `envconfig:"COMPRESS_WITH_GZ"`
+	ExportTempDir       string `envconfig:"EXPORT_TEMP_DIR" default:"/tmp"`
+	SkipSystemDatabases bool   `envconfig:"SKIP_SYSTEM_DATABASES" default:"true"`
+	CompressWithGz      bool   `envconfig:"COMPRESS_WITH_GZ" default:"true"`
 
-	LogLevel string `envconfig:"LOG_LEVEL"`
-}
-
-var DefaultConfig = Config{
-	LogLevel:     "info",
-	S3Region:     "us-west-1",
-	S3Endpoint:   "s3.us-west-1.amazonaws.com",
-	S3PathPrefix: "mysql-backup",
-
-	MySQLDumpBinary: "mysqldump",
-
-	MySQLUser: "root",
-	MySQLHost: "127.0.0.1",
-	MySQLPort: 3306,
-
-	ExportTempDir:       "/tmp",
-	SkipSystemDatabases: true,
-	CompressWithGz:      true,
+	LogLevel string `envconfig:"LOG_LEVEL" default:"info"`
 }
 
 func Load() *Config {
-	c := DefaultConfig
+	c := Config{}
 
-	err := envconfig.Process("", c)
+	err := envconfig.Process("", &c)
 	if err != nil {
 		logrus.Panicf("failed to parse log level, %+v", err)
 	}
